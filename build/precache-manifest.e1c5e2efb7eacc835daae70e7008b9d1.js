2622d73e078c989656e5677f46eb79c9659c4da9
AYUSH KUMAR MISHRA <114380089+CODEPANDA-1122@users.noreply.github.com>
2023-05-13 14:15:39 +0530

    Add files via upload

---
+++ build/precache-manifest.e1c5e2efb7eacc835daae70e7008b9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ef1d81cc7fdfc42524f0bed66fbb06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363227bd8cf398073861",
+    "url": "/static/css/main.9f27d892.chunk.css"
+  },
+  {
+    "revision": "b58bf110556bf3ca47fc",
+    "url": "/static/js/2.4fc8a9a1.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.4fc8a9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363227bd8cf398073861",
+    "url": "/static/js/main.4653cccc.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
